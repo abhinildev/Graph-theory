9f89cd0759e5f0cb10646101a5b6669841316726
abhinildev <140328202+abhinildev@users.noreply.github.com>
2024-04-03 21:23:17 +0530

    Update K4.py

--- K4.py
+++ K4.py
@@ -5,5 +5,6 @@ edgeList=[(1,2),(1,3),(1,4),(2,4),(2,3),(3,4)]
 G=nx.Graph()
 G.add_edges_from(edgeList)
 
-nx.draw_spring(G,with_labels=True)
+#nx.draw_spring(G,with_labels=True)
+nx.draw_planar(G,with_labels=True)
 plt.show()
