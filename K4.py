46b9060d64da4b347ad62f349b8e82510600fb99
abhinildev <140328202+abhinildev@users.noreply.github.com>
2024-04-03 21:21:29 +0530

    Create K4.py

---
+++ K4.py
@@ -0,0 +1,9 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+edgeList=[(1,2),(1,3),(1,4),(2,4),(2,3),(3,4)]
+G=nx.Graph()
+G.add_edges_from(edgeList)
+
+nx.draw_spring(G,with_labels=True)
+plt.show()
